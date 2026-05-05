f61b75b41c6fbdc29777f2c8d1488a2a541c845d
Art7849 <chaarthur2@gmail.com>
2026-05-05 11:35:52 +0200

    Add calibration coefficient arrays
    Replace unconstrained identifiers with typed, initialized float arrays for voltage and current calibration in main_carte_VI_RTOS.h. Adds facteur_tension[5], constante_tension[5], facteur_courant[5], and constante_courant[5] with initial values for five measurement points to provide explicit calibration constants and avoid implicit/undefined symbols.

--- Programme_carte_VI/src/main_carte_VI_RTOS.h
+++ Programme_carte_VI/src/main_carte_VI_RTOS.h
@@ -52,10 +52,10 @@ struct point_de_mesure
   float alpha;
 };
 
-facteur_tension[5] ;
-constante_tension[5] ;
-facteur_courant[5] ;
-constante_courant[5] ;
+float facteur_tension[5] ={-0.00806,-0.00111,-0.00847,-0.00771,-0.00736,};
+float constante_tension[5] ={1.13,0.997,1.14,1.12,1.1,};
+float facteur_courant[5] ={-0.0236,0.0503,-0.0066,-0.0241,-0.0138,};
+float constante_courant[5] ={1.09,0.916,1.04,1.08,1.07,};
 
 void onReceiveCan(int packetSize);
 void onReceiveSerial();
