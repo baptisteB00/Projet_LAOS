ef812a3318f3e9406d62ae9550513d33f1b9514d
baptisteB00 <superbaptiste3264@gmail.com>
2026-06-05 14:18:11 +0700

    harmonisation syntaxe + correction bugs courant
    carte_mesure_tension :
    - indentation tabs uniforme, espaces autour des opérateurs
    - correction mesure_courant_string : set_multiplexeur(6) au lieu de 5
    - correction mesure_courant_string : boucle i<4 au lieu de i<5 (évite buffer overflow)
    - correction mesure_courant_string : lecture_tension_multiplexeur(i+1) au lieu de (i)
    - séparateurs de sections /* ==== */
    - déclarations forward alignées
    
    carte_supervision :
    - correction décodage CAN_ID_RENVOI_COURANT_STRING : indices MSB décalés de 1 (bug)
    - correction data[8] hors tableau remplacé par data[6]<<8
    - ajout accolades sur case COURANT_STRING (variables locales dans switch)
    - indentation uniforme des nouveaux cases
    - suppression static int i mort dans reception()
    - }else if → } else if partout
    - alignement des déclarations de variables

--- carte_mesure_tension/src/main_carte_mesure_tension.cpp
+++ carte_mesure_tension/src/main_carte_mesure_tension.cpp
@@ -11,7 +11,10 @@
 #include <CAN.h>
 #include "can_id.h"
 
-#define MOYENNAGE 100
+#define MOYENNAGE       100
+#define FACTEUR_TENSION 0.081f
+#define FACTEUR_COURANT 0.003462f
+#define NUM_CARTE       13
 
 #define PIN_SELECTION_MULTIPLEXEUR_A 25
 #define PIN_SELECTION_MULTIPLEXEUR_B 33
@@ -22,86 +25,86 @@
 #define PIN_SORTIE_MULTIPLEXEUR_3 34
 #define PIN_SORTIE_MULTIPLEXEUR_4 35
 
-#define FACTEUR_TENSION 0.081f
-#define FACTEUR_COURANT 0.003462f
-#define NUM_CARTE 13
-
 typedef struct CANMessage_t
 {
-  unsigned int id = 0;
-  char len = 0;
-  unsigned char data[8] = {0};
+	unsigned int  id      = 0;
+	char          len     = 0;
+	unsigned char data[8] = {0};
 } CANMessage_t;
 
-CANMessage_t rxMsg;
+CANMessage_t  rxMsg;
 volatile bool canAvailable = false;
 
-void onReceiveCan(int packetSize);
-void set_multiplexeur(int chanel);
+void  onReceiveCan(int packetSize);
+void  envoie_message_CAN(CANMessage_t TxMsg);
+void  set_multiplexeur(int chanel);
 float lecture_tension_multiplexeur(char numero);
-void mesure_tension_string(char num_string);
-void mesure_courant_string(void);
-void reception(char ch);
-void envoi_num_carte();
+void  mesure_tension_string(char num_string);
+void  mesure_courant_string(void);
+void  reception(char ch);
+void  envoi_num_carte(void);
 
-void setup(){
-
-
+/* ==================================================================== */
+void setup()
+{
 	Serial.begin(115200);
-
 	Serial.println("Carte mesure de tension");
 
-	  // start the CAN bus at 10 kbps
-	if (!CAN.begin(10E3)) {
-	    Serial.println("Starting CAN failed!");
-	    while(1);
+	if (!CAN.begin(10E3))
+	{
+		Serial.println("Starting CAN failed!");
+		while (1);
 	}
 
 	CAN.onReceive(onReceiveCan);
-	pinMode(PIN_SELECTION_MULTIPLEXEUR_A,OUTPUT);
-	pinMode(PIN_SELECTION_MULTIPLEXEUR_B,OUTPUT);
-	pinMode(PIN_SELECTION_MULTIPLEXEUR_C,OUTPUT);
 
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_A,LOW);
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_B,LOW);
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_C,LOW);
+	pinMode(PIN_SELECTION_MULTIPLEXEUR_A, OUTPUT);
+	pinMode(PIN_SELECTION_MULTIPLEXEUR_B, OUTPUT);
+	pinMode(PIN_SELECTION_MULTIPLEXEUR_C, OUTPUT);
 
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_A, LOW);
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_B, LOW);
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_C, LOW);
 }
 
-
-void loop(){
+/* ==================================================================== */
+void loop()
+{
 	CANMessage_t rxMsgLocal;
-	if (canAvailable == true){
+	if (canAvailable == true)
+	{
 		rxMsgLocal = rxMsg;
-	    if (rxMsgLocal.id == CAN_ID_DEMANDE_TENSION_STRING)
-	    {
-		mesure_tension_string(rxMsgLocal.data[0]);
-	    }
-	    else if (rxMsgLocal.id == CAN_ID_DEMANDE_COURANT_STRING)
-	    {
-		mesure_courant_string();
-	    }
-	    else if (rxMsgLocal.id == CAN_ID_DEMANDE_NUM_CARTE)
-	    {
-		envoi_num_carte();
-	    }
+		if (rxMsgLocal.id == CAN_ID_DEMANDE_TENSION_STRING)
+		{
+			mesure_tension_string(rxMsgLocal.data[0]);
+		}
+		else if (rxMsgLocal.id == CAN_ID_DEMANDE_COURANT_STRING)
+		{
+			mesure_courant_string();
+		}
+		else if (rxMsgLocal.id == CAN_ID_DEMANDE_NUM_CARTE)
+		{
+			envoi_num_carte();
+		}
 		canAvailable = false;
 	}
 }
 
+/* ==================================================================== */
 void onReceiveCan(int packetSize)
 {
-    rxMsg.id = CAN.packetId();
-    rxMsg.len = CAN.packetDlc();
-    int i = 0;
-    while (CAN.available())
-    {
-        rxMsg.data[i] = CAN.read();
-        i++;
-    }
+	rxMsg.id  = CAN.packetId();
+	rxMsg.len = CAN.packetDlc();
+	int i = 0;
+	while (CAN.available())
+	{
+		rxMsg.data[i] = CAN.read();
+		i++;
+	}
 	canAvailable = true;
 }
 
+/* ==================================================================== */
 void envoie_message_CAN(CANMessage_t TxMsg)
 {
 	CAN.beginPacket(TxMsg.id);
@@ -109,219 +112,193 @@ void envoie_message_CAN(CANMessage_t TxMsg)
 	CAN.endPacket();
 }
 
+/* ==================================================================== */
 void serialEvent()
 {
-  while (Serial.available() > 0) // tant qu'il y a des caractères à lire
-  {
-    reception(Serial.read());
-  }
+	while (Serial.available() > 0)
+	{
+		reception(Serial.read());
+	}
 }
 
+/* ==================================================================== */
 void reception(char ch)
 {
-  static String chaine = "";
-  String commande;
-  String valeur;
-  int index, length;
+	static String chaine = "";
+	String commande;
+	String valeur;
+	int index, length;
 
-  if ((ch == 13) or (ch == 10))
-  {
-    index = chaine.indexOf(' ');
-    length = chaine.length();
+	if ((ch == 13) or (ch == 10))
+	{
+		index  = chaine.indexOf(' ');
+		length = chaine.length();
 
-    if (index == -1)
-    {
-      commande = chaine;
-      valeur = "";
-    }
-    else
-    {
-      commande = chaine.substring(0, index);
-      valeur = chaine.substring(index + 1, length);
-    }
+		if (index == -1)
+		{
+			commande = chaine;
+			valeur   = "";
+		}
+		else
+		{
+			commande = chaine.substring(0, index);
+			valeur   = chaine.substring(index + 1, length);
+		}
 
-    if (commande == "R")
-    {
-	mesure_tension_string(valeur.toInt());
-    }else if(commande == "T"){
-	mesure_courant_string();
+		if (commande == "R")
+		{
+			mesure_tension_string(valeur.toInt());
+		}
+		else if (commande == "T")
+		{
+			mesure_courant_string();
+		}
+		else if (commande == "N")
+		{
+			envoi_num_carte();
+		}
+		chaine = "";
 	}
-    else if (commande == "N")
-    {
-	envoi_num_carte();
-    }
-    chaine = "";
-  }
-  else
-  {
-    chaine += ch;
-  }
+	else
+	{
+		chaine += ch;
+	}
 }
 
 /**
- * @brief Mesure les 5 tensions et le courant d'un string.
- *
- * Sélectionne chaque canal du multiplexeur (1 à 6), lit la tension via ADC
- * (moyenne de `MOYENNAGE` lectures), applique le facteur d'échelle, puis
- * envoie 6 trames CAN (ID=51) avec l'indice et la valeur encodée.
- * @param num_string Numéro du string (1 à 4, correspond à la sortie multiplexeur).
+ * @brief Mesure les 5 tensions d'un string.
+ * @param num_string Numéro du string (1 à 4).
  */
-void mesure_tension_string(char num_string){
+void mesure_tension_string(char num_string)
+{
 	CANMessage_t txMsg;
 	CANMessage_t marqueurMsg;
 	marqueurMsg.len = 0;
 	float mesure[5];
-	float facteur;
-	if(num_string < 1 || num_string > 4) return;
-	for (int i =0 ; i<5;i++){
-		set_multiplexeur(i+1);
+
+	if (num_string < 1 || num_string > 4) return;
+
+	for (int i = 0; i < 5; i++)
+	{
+		set_multiplexeur(i + 1);
 		mesure[i] = lecture_tension_multiplexeur(num_string) * FACTEUR_TENSION;
 	}
 
-	/* Marqueur de début de rafale */
 	marqueurMsg.id = CAN_ID_DEBUT_TRANSMISSION;
 	envoie_message_CAN(marqueurMsg);
+
 	txMsg.len = 4;
-	txMsg.id = CAN_ID_RENVOI_TENSION_STRING;
-	for (int i =0 ; i<5 ; i++){
-		int valeur_int = (int)(mesure[i] * 100); // encodage x100
-		txMsg.data[0] = num_string;
-		txMsg.data[1] = i;                    // indice : 0-4 = tensions, 5 = courant
-		txMsg.data[2] = valeur_int / 256;         // octet fort
-		txMsg.data[3] = valeur_int % 256;         // octet faible
-		Serial.printf("numero de string : %d , numero de panneau : %d , tension = %f\n\r",num_string,i+1,mesure[i]);
+	txMsg.id  = CAN_ID_RENVOI_TENSION_STRING;
+	for (int i = 0; i < 5; i++)
+	{
+		int valeur_int = (int)(mesure[i] * 100);
+		txMsg.data[0]  = num_string;
+		txMsg.data[1]  = i;
+		txMsg.data[2]  = valeur_int / 256;
+		txMsg.data[3]  = valeur_int % 256;
+		Serial.printf("string %d, panneau %d, tension = %f\n\r", num_string, i + 1, mesure[i]);
 		envoie_message_CAN(txMsg);
 	}
 
-
-	/* Marqueur de fin de rafale */
 	marqueurMsg.id = CAN_ID_FIN_TRANSMISSION;
 	envoie_message_CAN(marqueurMsg);
 }
-void mesure_courant_string(void){
+
+/**
+ * @brief Mesure le courant des 4 strings et envoie une trame CAN de 8 octets.
+ */
+void mesure_courant_string(void)
+{
 	CANMessage_t txMsg;
 	CANMessage_t marqueurMsg;
 	marqueurMsg.len = 0;
 	float mesure[4];
-	int valeur_int;
 
-	for (int i =0 ; i<4;i++){
+	for (int i = 0; i < 4; i++)
+	{
 		set_multiplexeur(6);
-		mesure[i] = lecture_tension_multiplexeur(i) * FACTEUR_COURANT;
+		mesure[i] = lecture_tension_multiplexeur(i + 1) * FACTEUR_COURANT;
 	}
 
-	/* Marqueur de début de rafale */
 	marqueurMsg.id = CAN_ID_DEBUT_TRANSMISSION;
 	envoie_message_CAN(marqueurMsg);
+
 	txMsg.len = 8;
-	txMsg.id = CAN_ID_RENVOI_COURANT_STRING;
-	for (int i = 0 ; i< 4 ; i++){
-		valeur_int = (int)(mesure[i] * 100); // encodage x100
-		txMsg.data[2*i]   = valeur_int / 256;         // octet fort
-		txMsg.data[2*i+1] = valeur_int % 256;         // octet faible
-		Serial.printf("courant %d = %f\n\r",i,mesure[i]);
-	 }
+	txMsg.id  = CAN_ID_RENVOI_COURANT_STRING;
+	for (int i = 0; i < 4; i++)
+	{
+		int valeur_int        = (int)(mesure[i] * 100);
+		txMsg.data[2 * i]     = valeur_int / 256;
+		txMsg.data[2 * i + 1] = valeur_int % 256;
+		Serial.printf("courant %d = %f\n\r", i + 1, mesure[i]);
+	}
 	envoie_message_CAN(txMsg);
 
-
-	/* Marqueur de fin de rafale */
 	marqueurMsg.id = CAN_ID_FIN_TRANSMISSION;
 	envoie_message_CAN(marqueurMsg);
 }
+
 /**
  * @brief Configure le multiplexeur pour sélectionner un canal.
  * @param chanel Canal à sélectionner : 1=V1, 2=V2, 3=V3, 4=V4, 5=V5, 6=courant.
  */
-void set_multiplexeur(int chanel){
-	bool inA,inB,inC; //variable pour stocker l'etat souhaiter des pin de configuration du multiplexeur
+void set_multiplexeur(int chanel)
+{
+	bool inA, inB, inC;
 
-	switch(chanel){
-		case 1 ://case pour selectionner la tension 1 
-			inA = HIGH;
-			inB = HIGH;
-			inC = LOW;
-		break;
-		case 2://case pour selectionner la tension 2 
-			inA = LOW;
-			inB = LOW;
-			inC = LOW;
-		break;
-		case 3://case pour selectionner la tension 3 
-			inA = HIGH;
-			inB = LOW;
-			inC = LOW;
-		break;
-		case 4://case pour selectionner la tension 4 
-			inA = LOW;
-			inB = HIGH;
-			inC = LOW;
-		break;
-
-		case 5://case pour selectionner la tension 5 
-			inA = LOW;
-			inB = LOW;
-			inC = HIGH;
-		break;
-		case 6://case pour selectionner le courant
-			inA = HIGH;
-			inB = LOW;
-			inC = HIGH;
-		break;
-		default:// par defaut le multiplexeur mesure la tension 1 si on entre un numero non valide
-			inA = HIGH;
-			inB = HIGH;
-			inC = LOW;
-		break;
-
+	switch (chanel)
+	{
+		case 1:  inA = HIGH; inB = HIGH; inC = LOW;  break;
+		case 2:  inA = LOW;  inB = LOW;  inC = LOW;  break;
+		case 3:  inA = HIGH; inB = LOW;  inC = LOW;  break;
+		case 4:  inA = LOW;  inB = HIGH; inC = LOW;  break;
+		case 5:  inA = LOW;  inB = LOW;  inC = HIGH; break;
+		case 6:  inA = HIGH; inB = LOW;  inC = HIGH; break;
+		default: inA = HIGH; inB = HIGH; inC = LOW;  break;
 	}
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_A,inA);
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_B,inB);
-	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_C,inC);
+
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_A, inA);
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_B, inB);
+	digitalWrite(PIN_SELECTION_MULTIPLEXEUR_C, inC);
 }
 
 /**
- * @brief Lit la valeur ADC à la sortie du multiplexeur (moyenne de `MOYENNAGE` lectures).
+ * @brief Lit la tension à la sortie du multiplexeur (moyenne de MOYENNAGE lectures).
  * @param numero Numéro du string (1 à 4) → sélectionne le pin ADC correspondant.
- * @return Valeur ADC brute moyennée, ou -1.0f si le numéro est invalide.
+ * @return Tension en volts moyennée.
  */
-float lecture_tension_multiplexeur(char numero){
+float lecture_tension_multiplexeur(char numero)
+{
 	float lecture_tension = 0;
-	char pin;
-	switch(numero){
-		case 1:
-			pin = PIN_SORTIE_MULTIPLEXEUR_1;
-		break;
-		case 2:
-			pin = PIN_SORTIE_MULTIPLEXEUR_2;
-		break;
-		case 3:
-			pin = PIN_SORTIE_MULTIPLEXEUR_3;
-		break;
-		case 4:
-			pin = PIN_SORTIE_MULTIPLEXEUR_4;
-		break;
-		default:
-			pin = PIN_SORTIE_MULTIPLEXEUR_1;
-		break;
+	char  pin;
+
+	switch (numero)
+	{
+		case 1:  pin = PIN_SORTIE_MULTIPLEXEUR_1; break;
+		case 2:  pin = PIN_SORTIE_MULTIPLEXEUR_2; break;
+		case 3:  pin = PIN_SORTIE_MULTIPLEXEUR_3; break;
+		case 4:  pin = PIN_SORTIE_MULTIPLEXEUR_4; break;
+		default: pin = PIN_SORTIE_MULTIPLEXEUR_1; break;
 	}
 
-	for (int i =0 ;i< MOYENNAGE;i++){
-		lecture_tension += analogReadMilliVolts(pin)*48.52f/1000.0f;
+	for (int i = 0; i < MOYENNAGE; i++)
+	{
+		lecture_tension += analogReadMilliVolts(pin) * 48.52f / 1000.0f;
 	}
-
-	lecture_tension/=MOYENNAGE;
+	lecture_tension /= MOYENNAGE;
 
 	return lecture_tension;
 }
 
 /**
- * @brief Envoie le numéro de cette carte (13) sur le bus CAN (ID=10).
+ * @brief Envoie le numéro de cette carte (13) sur le bus CAN.
  */
-void envoi_num_carte(){
+void envoi_num_carte(void)
+{
 	CANMessage_t txMsg;
-	txMsg.id = CAN_ID_RENVOI_NUM_CARTE;
+	txMsg.id      = CAN_ID_RENVOI_NUM_CARTE;
 	txMsg.data[0] = NUM_CARTE;
-	txMsg.len = 1;
+	txMsg.len     = 1;
 	envoie_message_CAN(txMsg);
 	Serial.println("numero de carte envoye");
 }
--- carte_supervision/src/main_supervision.cpp
+++ carte_supervision/src/main_supervision.cpp
@@ -4,55 +4,53 @@
 
 typedef struct CANMessage
 {
-  bool extented = false;
-  bool RTR = false;
-  unsigned int id = 0;
-  char len = 0;
+  bool         extented = false;
+  bool         RTR      = false;
+  unsigned int id       = 0;
+  char         len      = 0;
   unsigned char data[8] = {0};
 } CANMessage;
 
-CANMessage rxMsg;
+CANMessage    rxMsg;
 volatile bool canAvailable = false;
 
 void onReceive(int packetSize);
 void reception(char ch);
 
+/* ==================================================================== */
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial);
 
   Serial.println("CAN Receiver");
 
-  // start the CAN bus at 10 kbps
   if (!CAN.begin(10E3))
   {
     Serial.println("Starting CAN failed!");
-    while (1)
-      ;
+    while (1);
   }
-  // register the receive callback
   CAN.onReceive(onReceive);
 }
 
+/* ==================================================================== */
 void loop()
 {
-  int numero_carte_recu ;
-  float tension = 0.0f;
-  float courant = 0.0f;
-  float humidite = 0.0f;
+  int   numero_carte_recu;
+  float tension         = 0.0f;
+  float courant         = 0.0f;
+  float humidite        = 0.0f;
   float temperature_ext = 0.0f;
-  float irradiance = 0.0f;
-  int numero_carte = 0;
-  int temperature_panneau = 0;
-  char num_string;
-  char num_panneau;
+  float irradiance      = 0.0f;
+  int   numero_carte    = 0;
+  int   temperature_panneau = 0;
+  char  num_string;
+  char  num_panneau;
+
   if (canAvailable == true)
   {
     switch (rxMsg.id)
     {
-
     case CAN_ID_DEBUT_TRANSMISSION:
       Serial.println("0");
       break;
@@ -62,13 +60,13 @@ void loop()
       break;
 
     case CAN_ID_RENVOI_NUM_CARTE:
-    numero_carte_recu = rxMsg.data[0];
-    Serial.printf("0;%d\n\r", numero_carte_recu);
-    break;
+      numero_carte_recu = rxMsg.data[0];
+      Serial.printf("0;%d\n\r", numero_carte_recu);
+      break;
 
     case CAN_ID_RENVOI_TEMP_PANNEAU:
       temperature_panneau = rxMsg.data[1];
-      if (rxMsg.data[0] == 0) // data[0]=0 -> température négative (signe envoyé par la carte VI)
+      if (rxMsg.data[0] == 0)
       {
         temperature_panneau = -temperature_panneau;
       }
@@ -78,80 +76,68 @@ void loop()
       break;
 
     case CAN_ID_RENVOI_MESURE_VI:
-      tension = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
-      courant = (rxMsg.data[2] * 256 + rxMsg.data[3]) / 100.0f;
+      tension      = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
+      courant      = (rxMsg.data[2] * 256 + rxMsg.data[3]) / 100.0f;
       numero_carte = rxMsg.data[4];
       Serial.printf("1;%d;%.2f;%.2f\n\r", numero_carte, tension, courant);
       Serial.println();
-
       break;
 
     case CAN_ID_RENVOI_HUMIDITE:
       humidite = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
       Serial.printf("11;%.2f", humidite);
       Serial.println();
-
       break;
 
     case CAN_ID_RENVOI_TEMPERATURE:
       temperature_ext = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
       Serial.printf("12;%.2f", temperature_ext);
       Serial.println();
-
       break;
 
     case CAN_ID_RENVOI_IRRADIANCE:
       irradiance = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
       Serial.printf("13;%.2f", irradiance);
       Serial.println();
-
       break;
 
     case CAN_ID_RENVOI_HUM_IRR_TEMP_EXT:
-      humidite = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
+      humidite        = (rxMsg.data[0] * 256 + rxMsg.data[1]) / 100.0f;
       temperature_ext = (rxMsg.data[2] * 256 + rxMsg.data[3]) / 100.0f;
-      irradiance = (rxMsg.data[4] * 256 + rxMsg.data[5]) / 100.0f;
-
+      irradiance      = (rxMsg.data[4] * 256 + rxMsg.data[5]) / 100.0f;
       Serial.printf("10;%.2f;%.2f;%.2f", humidite, temperature_ext, irradiance);
       Serial.println();
+      break;
 
+    case CAN_ID_RENVOI_TENSION_STRING:
+      tension     = (rxMsg.data[3] | rxMsg.data[2] << 8) / 100.0f;
+      num_string  = rxMsg.data[0];
+      num_panneau = rxMsg.data[1];
+      Serial.printf("20;%d;%d;%4.2f", num_string, num_panneau, tension);
+      Serial.println();
       break;
-      case CAN_ID_RENVOI_TENSION_STRING:
-        tension = (rxMsg.data[3] | rxMsg.data[2]<<8)/100.0f;
-        num_string = rxMsg.data[0];
-        num_panneau = rxMsg.data[1];
-        Serial.printf("20;%d;%d;%4.2f",num_string,num_panneau,tension);
-        Serial.println();
-      break;
-      case CAN_ID_RENVOI_COURANT_STRING:
-        float courant_string_1 = (rxMsg.data[1] | rxMsg.data[2]<<8)/100.0f;
-        float courant_string_2 = (rxMsg.data[3] | rxMsg.data[4]<<8)/100.0f;
-        float courant_string_3 = (rxMsg.data[5] | rxMsg.data[6]<<8)/100.0f;
-        float courant_string_4 = (rxMsg.data[7] | rxMsg.data[8]<<8)/100.0f;
-        Serial.printf("21;%2.2f;%2.2f;%2.2f;%2.2f\n\r",courant_string_1,courant_string_2,courant_string_3,courant_string_4);
-        Serial.println();
+
+    case CAN_ID_RENVOI_COURANT_STRING:
+    {
+      float courant_string_1 = (rxMsg.data[1] | rxMsg.data[0] << 8) / 100.0f;
+      float courant_string_2 = (rxMsg.data[3] | rxMsg.data[2] << 8) / 100.0f;
+      float courant_string_3 = (rxMsg.data[5] | rxMsg.data[4] << 8) / 100.0f;
+      float courant_string_4 = (rxMsg.data[7] | rxMsg.data[6] << 8) / 100.0f;
+      Serial.printf("21;%2.2f;%2.2f;%2.2f;%2.2f\n\r",
+                    courant_string_1, courant_string_2,
+                    courant_string_3, courant_string_4);
+      Serial.println();
       break;
     }
-  /* 
-    printf("ID  = %d\n", rxMsg.id);
-    printf("Len = %d\n", rxMsg.len);
-    if (rxMsg.len > 0)
-    {
-      Serial.print("Data(s) : ");
-      for (int i = 0; i < rxMsg.len; i++)
-      {
-        Serial.print(rxMsg.data[i]);
-        Serial.print(" ");
-      }
-     Serial.println();
-    }*/
+    }
     canAvailable = false;
   }
 }
 
+/* ==================================================================== */
 void onReceive(int packetSize)
 {
-  rxMsg.id = CAN.packetId();
+  rxMsg.id  = CAN.packetId();
   rxMsg.len = CAN.packetDlc();
   int i = 0;
   while (CAN.available())
@@ -162,17 +148,18 @@ void onReceive(int packetSize)
   canAvailable = true;
 }
 
+/* ==================================================================== */
 void serialEvent()
 {
-  while (Serial.available() > 0) // tant qu'il y a des caractères à lire
+  while (Serial.available() > 0)
   {
     reception(Serial.read());
   }
 }
 
+/* ==================================================================== */
 void reception(char ch)
 {
-  static int i = 0;
   static String chaine = "";
   String commande;
   String valeur;
@@ -180,18 +167,18 @@ void reception(char ch)
 
   if ((ch == 13) or (ch == 10))
   {
-    index = chaine.indexOf(' ');
+    index  = chaine.indexOf(' ');
     length = chaine.length();
 
     if (index == -1)
     {
       commande = chaine;
-      valeur = "";
+      valeur   = "";
     }
     else
     {
       commande = chaine.substring(0, index);
-      valeur = chaine.substring(index + 1, length);
+      valeur   = chaine.substring(index + 1, length);
     }
 
     if (commande == "R")
@@ -228,11 +215,12 @@ void reception(char ch)
       CAN.beginPacket(CAN_ID_DEMANDE_TENSION_STRING);
       CAN.write(valeur.toInt());
       CAN.endPacket();
-    }else if (commande == "C"){
+    }
+    else if (commande == "C")
+    {
       CAN.beginPacket(CAN_ID_DEMANDE_COURANT_STRING);
       CAN.endPacket();
     }
-
 
     chaine = "";
   }
@@ -241,4 +229,3 @@ void reception(char ch)
     chaine += ch;
   }
 }
-
